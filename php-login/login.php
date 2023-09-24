da2332f1d7a740dcba7f64114b7475f69c5e71e8
mrealan <marcelorealan19@gmail.com>
2023-09-23 21:50:08 -0300

    index casi terminado

---
+++ php-login/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="shortcut icon" href="assets/img/logo.png">
+    <title>Sisviansa</title>
+</head>
+
+<body>
+    <h2>Inicio de sesion</h2>
+    <form action="login.php" method="post">
+        <label for="usuario">Usuario</label>
+        <input type="text" required>
+        <br>
+        <label for="contrasena">Contraseña</label>
+        <input type="email" name="contrasena" id="">
+        <br>
+        <input type="submit" value="Ingresar">
+    </form>
+</body>
+
+</html>
