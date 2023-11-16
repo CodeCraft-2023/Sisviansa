5cb5e9367f031c160d9bdbe5105f22c991a493da
mrealan <marcelorealan19@gmail.com>
2023-11-16 15:22:25 -0300

    login y signup funcionales

--- php/database.php
+++ php/database.php
@@ -1,34 +1,14 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "sisviansa";
 
-// Función "conectar" para establecer una conexión a la base de datos.
-function conectar()
-{
-    $servername = "localhost"; // Servidor de la base de datos
-    $username = "root"; // Nombre de usuario de la base de datos
-    $password = ""; // Contraseña de la base de datos
-    $dbname = "sisviansa"; // Nombre de la base de datos
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
 
-    // Crear una nueva instancia de la clase mysqli para establecer la conexión a la base de datos.
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Comprobar si se ha producido un error de conexión.
-    if ($conn->connect_error) {
-        die("Error de conexión: " . $conn->connect_error);
-    }
-
-    return $conn; // Devolver la conexión establecida
+// Verificar conexión
+if ($conn->connect_error) {
+    die("La conexión falló: " . $conn->connect_error);
 }
-
-// Llamar a la función "conectar" para obtener una conexión a la base de datos.
-$conexion = conectar();
-
-// Función "desconectar" para cerrar una conexión a la base de datos.
-function desconectar($conn)
-{
-    $conn->close(); // Cierra la conexión pasada como argumento.
-}
-
-// Llamar a la función "desconectar" para cerrar la conexión previamente establecida en "$conexion".
-desconectar($conexion); // Donde "$conexion" es el objeto de conexión obtenido previamente.
-
 ?>
